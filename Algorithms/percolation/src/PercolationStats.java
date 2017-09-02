549758a62214fed53f846bcaa86464b497f26690
hankun01 <hankun01@baidu.com>
2017-09-02 16:36:28 +0800

    initialize the project

---
+++ Algorithms/percolation/src/PercolationStats.java
@@ -0,0 +1,34 @@
+/**
+ * Created by hankun on 2017/9/2.
+ */
+public class PercolationStats {
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 0.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 0.0;
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 0.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 0.0;
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+        System.out.println("percolation stats test");
+    }
+}
